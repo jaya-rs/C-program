23a4539af6f69c66c418cfba18b99afd1fbb627b
jaya-rs <rsjayaa2008@gmail.com>
2026-03-27 17:41:53 +0530

    Add files via upload

---
+++ 5A.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a[5],i;
+printf("Enter 5 numbers/n");
+for (i=0;i<5;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("List of even numbers/n");
+for(i=0;i<5;i++0
+{
+if(a[i]%2==0)
+{
+printf("d",a[i]);
+}
+}
+return 0;
+}
